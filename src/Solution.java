5773e965624599c13e7e524825561cdb777dcd34
Arijit Sarkar <arijit@Arijits-MacBook-Air.local>
2022-06-02 11:48:22 +0530

    Initial Commit

---
+++ src/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) 
+    {
+    	if (n==0)
+    		return 1;
+    	if (n==1)
+    		return 2;
+    	if (n%2==0)
+    		return utopianTree(n-1) +1;
+    	else if(n%2==1)
+    		return utopianTree(n-1) *2;
+    	else 
+    	return n;
+    	
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+    	BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
